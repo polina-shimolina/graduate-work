9e3133a9d736f329a7739758495d65f4454f98fc
polina-shimolina <shimolina.polina@gmail.com>
2024-05-02 18:19:26 +0300

    Базовая авторизация

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import Header from './components/Header';
+/* import Header from './components/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {Provider} from "react-redux"
 import Photos from './components/Photos';
@@ -23,3 +23,53 @@ function App() {
 }
 
 export default App;
+ */
+import React, { useState, useEffect } from 'react';
+
+const UserProfile = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [dateJoined, setDateJoined] = useState('');
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetch('/api/user')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`Something went wrong: code ${response.status}`);
+        }
+      })
+      .then(({ data }) => {
+        setFirstName(data.first_name);
+        setLastName(data.last_name);
+        setUsername(data.username);
+        setEmail(data.email);
+        setDateJoined(data.date_joined);
+      })
+      .catch(error => {
+        console.log(error);
+        setError('Ошибка, подробности в консоли');
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <div className="Profile">
+          <h1>{firstName} {lastName}</h1>
+          <h2>{username}</h2>
+          <p>email: {email}</p>
+          <p>Профиль создан {dateJoined}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserProfile;
--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -6,8 +6,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from myapi.serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all().order_by('name')
@@ -33,6 +35,8 @@ def upload_photo(request):
     
 
 @api_view()
+@permission_classes([IsAuthenticated])
+@authentication_classes([BasicAuthentication])
 def user(request: Request):
     return Response({
         'data': UserSerializer(request.user).data
