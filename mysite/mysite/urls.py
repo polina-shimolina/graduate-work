dafab80067d3db0544d78933bb469a072986dbde
polina-shimolina <shimolina.polina@gmail.com>
2024-05-05 00:01:00 +0300

    Добавлена генерация сваггера

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from django.contrib import admin
 from django.urls import path, include
 from myapi import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+    title="Сервис сегментации изображений",
+    default_version='v1',
+    description="API для сервиса сегментации объектов на изображении",
+    terms_of_service="https://www.example.com/policies/terms/",
+    contact=openapi.Contact(email="shimolinapk@gamil.com.com"),
+    license=openapi.License(name="Лицензия BSD"),
+),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -37,5 +54,6 @@ urlpatterns = [
     path('api/upload/', views.FileUploadView.as_view(), name='file-upload'),
     path('api/team/', views.TeamCreateView.as_view(), name='team-create'),
     path('api/team/<int:team_id>/users/', views.TeamUsersView.as_view(), name='team_users'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
  
