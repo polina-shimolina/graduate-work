1beb3ecf74c07c5a02959615cbb73cce9ac2584b
nazgull08 <nazgull08@protonmail.com>
2024-05-18 12:40:28 +0000

    shell fix

--- shell.nix
+++ shell.nix
@@ -1,29 +1,25 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  buildInputs = [
-    (pkgs.python3.withPackages (ps: with ps; [
-      asgiref
-      django
-      djangorestframework
-      djangorestframework-simplejwt
-      drf-yasg
-      inflection
-      packaging
-      pillow
-      pyjwt
-      pytz
-      pyyaml
-      setuptools
-      sqlparse
-      tzdata
-      uritemplate
-    ]))
-    pkgs.nodejs
-  ];
+(pkgs.buildFHSUserEnv {
+  name = "pipzone";
+  targetPkgs = pkgs: (with pkgs; [
+    python3
+    python3Packages.pip
+    python3Packages.virtualenv
+    nodejs
+  ]);
+  runScript = ''
+    # Создаем и активируем виртуальную среду
+    virtualenv venv
+    source venv/bin/activate
 
-  shellHook = ''
+    # Устанавливаем пакеты через pip
+    pip install asgiref django djangorestframework djangorestframework-simplejwt numpy matplotlib drf-yasg inflection packaging pandas pillow pyjwt pytz pyyaml setuptools scikit-learn sqlparse tzdata torch torchvision uritemplate torchviz opencv-python
+
+    # Меняем приглашение командной строки
     export PS1="\\[\033[34m\][nix-shell:\\w] \\[\033[0m\]$ "
+    
+    # Запускаем bash для работы в новой среде
+    exec bash
   '';
-}
-
+}).env
